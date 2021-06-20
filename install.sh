1177f954a3b3e70a6a86f463f023059fbc49b403
FKGReborn <37043021+FKGReborn@users.noreply.github.com>
2021-06-20 12:53:41 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#opvc (Wegare remod fakegrafis)
+wget --no-check-certificate "https://raw.githubusercontent.com/FKGReborn/opvclinux/main/opvc.sh" -O /usr/bin/opvc
+wget --no-check-certificate "https://raw.githubusercontent.com/FKGReborn/opvclinux/main/autorekonek-opvc.sh" -O /usr/bin/autorekonek-opvc
+apt update && apt install lsof && apt install openvpn
+chmod +x /usr/bin/opvc
+chmod +x /usr/bin/autorekonek-opvc
+rm -r ~/install.sh
+mkdir -p ~/akun/
+touch ~/akun/opvc.txt
+sleep 2
+echo "install selesai"
+echo "untuk memulai tools silahkan jalankan perintah 'opvc'"
+
+				
