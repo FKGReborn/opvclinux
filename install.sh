451471894b1de03e31f142efbbecf2aff2bcf396
FKGReborn <37043021+FKGReborn@users.noreply.github.com>
2021-07-19 18:52:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #opvc (Wegare remod fakegrafis)
 wget --no-check-certificate "https://raw.githubusercontent.com/FKGReborn/opvclinux/main/opvc.sh" -O /usr/bin/opvc
 wget --no-check-certificate "https://raw.githubusercontent.com/FKGReborn/opvclinux/main/autorekonek-opvc.sh" -O /usr/bin/autorekonek-opvc
-apt update && apt install lsof && apt install openvpn
+apt update && apt install lsof && apt install openvpn curl
 chmod +x /usr/bin/opvc
 chmod +x /usr/bin/autorekonek-opvc
 rm -r ~/install.sh
